a042a7cc0a32f42e53df37f89fd4165be2728500
Vandewaeter Fabio <vandewaeter.fabio@gmail.com>
2025-07-31 21:45:39 +0200

    render a quad

--- src/main/java/com/terrescalmes/core/GameEngine.java
+++ src/main/java/com/terrescalmes/core/GameEngine.java
@@ -56,12 +56,22 @@ public class GameEngine {
 
     public void init(Window window, Scene scene, Render render) {
         float[] positions = new float[] {
-                0.0f, 0.5f, 0.0f,
+                -0.5f, 0.5f, 0.0f,
                 -0.5f, -0.5f, 0.0f,
-                0.5f, -0.5f, 0.0f
+                0.5f, -0.5f, 0.0f,
+                0.5f, 0.5f, 0.0f,
         };
-        Mesh mesh = new Mesh(positions, 3);
-        scene.addMesh("triangle", mesh);
+        float[] colors = new float[] {
+                0.5f, 0.0f, 0.0f,
+                0.0f, 0.5f, 0.0f,
+                0.0f, 0.0f, 0.5f,
+                0.0f, 0.5f, 0.5f,
+        };
+        int[] indices = new int[] {
+                0, 1, 3, 3, 1, 2,
+        };
+        Mesh mesh = new Mesh(positions, colors, indices);
+        scene.addMesh("quad", mesh);
     }
 
     private void gameLoop() {
